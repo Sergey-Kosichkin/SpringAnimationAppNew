ccb54ddefc0e3761f78eb3eed4dcbe632b32065b
Sergey Kosichkin <d978827@icloud.com>
2023-01-12 19:37:55 +0300

    Initial Commit

---
+++ SpringAnimationAppNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringAnimationAppNew
+//
+//  Created by Sergey Kosichkin on 12.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
